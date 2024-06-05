ee346c5f712acc0cfb2822ad95114f087b21064b
Mikolaj Figurski <miko.f80@gmail.com>
2024-06-05 01:37:20 -0400

    refactor main script

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod read_csv;
 fn main() {
     let mut args = std::env::args();
     if args.len() < 2 {
-        println!("Usage: xmeans <data_file_path> [-k <number>]");
+        println!("Usage: xmeans <data_file_path> [-k <number>] [--delim <string>]");
         std::process::exit(1);
     }
     let data_file_path = args.nth(1).unwrap();
@@ -14,17 +14,27 @@ fn main() {
         std::process::exit(1);
     }
 
-    // read the data file into flat numeric vector
-    let (data, shape) = read_csv::read_csv_data(&data_file_path, b';');
-
-    // check for -k <number> flag
+    let mut delim = b',';
     let mut k = 0;
     while let Some(arg) = args.next() {
-        if arg == "-k" || arg == "--k" {
-            k = args.next().unwrap().parse().unwrap();
-            break;
+        match arg.as_str() {
+            // check for -k <number> flag
+            "-k" | "--k" => {
+                k = args.next().unwrap().parse().unwrap();
+            }
+            // check for --delim <string> flag
+            "--delim" => {
+                delim = args.next().unwrap().as_bytes()[0];
+            }
+            _ => {
+                println!("Error: unknown flag {}", arg);
+                std::process::exit(1);
+            }
         }
     }
+
+    // read the data file into flat numeric vector
+    let (data, shape) = read_csv::read_csv_data(&data_file_path, delim);
     if k > 0 {
         run_kmeans(&data, shape, k);
     } else {
@@ -37,26 +47,30 @@ fn run_kmeans(data: &Vec<f64>, shape: usize, k: usize) {
     let kmean = KMeans::new(data.clone(), data_len, shape);
     let result = kmean.kmeans_lloyd(k, 100, KMeans::init_kmeanplusplus, &KMeansConfig::default());
     let wrapped_centroids: Vec<&[f64]> = result.centroids.chunks(shape).collect();
-    for (_, centroid) in wrapped_centroids.iter().enumerate() {
-        let fmtted_centroid: Vec<String> = centroid.iter().map(|x| format!("{:.0}", x)).collect();
-        // print with comma separated values
-        println!("{}", fmtted_centroid.join(","));
-    }
+    print_centroids(&wrapped_centroids);
 }
 
 fn run_xmeans(data: &Vec<f64>, shape: usize) {
     let data_len = data.len() / shape;
     println!("Data shape: {} by {}", shape, data_len);
     // kmeans
-    let k = 30;
+    let k = 20;
     let kmean = KMeans::new(data.clone(), data_len, shape);
     let result = kmean.kmeans_lloyd(k, 100, KMeans::init_kmeanplusplus, &KMeansConfig::default());
 
     let wrapped_centroids: Vec<&[f64]> = result.centroids.chunks(shape).collect();
-    println!("wrapped centroids: {:?}", wrapped_centroids);
+    print_centroids(&wrapped_centroids);
     let wrapped_data: Vec<&[f64]> = data.chunks(shape).collect();
     let bic = bic::compute_bic(&wrapped_data, &wrapped_centroids, result.assignments);
     println!("Computed BIC: {:?}", bic);
     // not implemented
     unimplemented!();
+}
+
+fn print_centroids(c: &Vec<&[f64]>) {
+    for centroid in c.iter() {
+        let fmtted_centroid: Vec<String> = centroid.iter().map(|x| format!("{:.0}", x)).collect();
+        // print with comma separated values
+        println!("{}", fmtted_centroid.join(","));
+    }
 }
