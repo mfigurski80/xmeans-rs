d4df1c0bc6941817c13e3e2a0d43332f88e5b3a7
Mikolaj Figurski <miko.f80@gmail.com>
2024-07-10 17:48:42 -0400

    style: pull out next_centroids func

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
-use kmeans::*;
 mod args;
 mod bic;
 mod read_csv;
+mod xmeans;
+
+use kmeans::*;
 
 fn main() {
-    let found = args::parse_args(std::env::args());
-    if found.is_err() {
-        eprintln!("{}", found.err().unwrap());
-        std::process::exit(1);
-    }
-    let args = found.unwrap();
+    let args = match args::parse_args(std::env::args()) {
+        Err(e) => {
+            eprintln!("{}", e);
+            std::process::exit(1);
+        }
+        Ok(args) => args,
+    };
 
     // read the data file into flat numeric vector
     let (data, shape) = read_csv::read_csv_data(args.file_path.as_str(), args.delim);
@@ -50,7 +53,7 @@ fn run_xmeans(data: &Vec<f64>, shape: usize, start_k: usize) {
     println!("Initial BIC: {:?}", bic);
 
     loop {
-        let next_centroids = next_centroids(&wrapped_data, kmean_result);
+        let next_centroids = xmeans::next_centroids(&wrapped_data, kmean_result);
         println!("Next Centroids: {:?}", next_centroids);
         break;
     }
@@ -64,54 +67,4 @@ fn print_centroids(c: &Vec<&[f64]>) {
         // print with comma separated values
         println!("{}", fmtted_centroid.join(","));
     }
-}
-
-/// Got some centroids? Returns a bigger set that potentially improves on
-/// them! Just train them and check the bic again to make sure
-fn next_centroids<'a>(wrapped_data: &Vec<&'a [f64]>, state: kmeans::KMeansState<f64>) -> Vec<f64> {
-    let mut next_centroids: Vec<f64> = vec![];
-
-    let shape = wrapped_data[0].len();
-    let centroids = state.centroids.chunks(shape);
-    for (cluster_i, centroid) in centroids.enumerate() {
-        let cluster_data = state
-            .assignments
-            .iter()
-            .enumerate()
-            .filter(|(_, assigned)| **assigned == cluster_i)
-            .flat_map(|(i, _)| wrapped_data[i])
-            .map(|x| *x)
-            .collect::<Vec<f64>>();
-        let cluster_data_len = cluster_data.len() / shape;
-        // attempt to split the cluster
-        let kmean = KMeans::new(cluster_data.clone(), cluster_data_len, shape);
-        let kmeans_result = kmean.kmeans_lloyd(
-            2,
-            100,
-            KMeans::init_random_partition,
-            &KMeansConfig::default(),
-        );
-        let wrapped_cluster_data: Vec<&[f64]> = cluster_data.chunks(shape).collect();
-        let old_bic = bic::compute_bic(
-            &wrapped_cluster_data,
-            &vec![centroid],
-            vec![0; cluster_data.len()],
-        );
-        let new_bic = bic::compute_bic(
-            &wrapped_cluster_data,
-            &kmeans_result.centroids.chunks(shape).collect(),
-            kmeans_result.assignments,
-        );
-        println!(
-            "Comparing centroids: {:?}({}) to {:?}({})?",
-            centroid, old_bic, kmeans_result.centroids, new_bic
-        );
-        if old_bic < new_bic {
-            next_centroids.extend(centroid.iter());
-        } else {
-            next_centroids.extend(kmeans_result.centroids.iter());
-        }
-    }
-
-    next_centroids
 }
---
+++ src/xmeans.rs
@@ -0,0 +1,55 @@
+use crate::bic::compute_bic;
+use kmeans::*;
+
+/// Got some centroids? Returns a bigger set that potentially improves on
+/// them! Just train them and check the bic again to make sure
+pub fn next_centroids<'a>(
+    wrapped_data: &Vec<&'a [f64]>,
+    state: kmeans::KMeansState<f64>,
+) -> Vec<f64> {
+    let mut next_centroids: Vec<f64> = vec![];
+
+    let shape = wrapped_data[0].len();
+    let centroids = state.centroids.chunks(shape);
+    for (cluster_i, centroid) in centroids.enumerate() {
+        let cluster_data = state
+            .assignments
+            .iter()
+            .enumerate()
+            .filter(|(_, assigned)| **assigned == cluster_i)
+            .flat_map(|(i, _)| wrapped_data[i])
+            .map(|x| *x)
+            .collect::<Vec<f64>>();
+        let cluster_data_len = cluster_data.len() / shape;
+        // attempt to split the cluster
+        let kmean = KMeans::new(cluster_data.clone(), cluster_data_len, shape);
+        let kmeans_result = kmean.kmeans_lloyd(
+            2,
+            100,
+            KMeans::init_random_partition,
+            &KMeansConfig::default(),
+        );
+        let wrapped_cluster_data: Vec<&[f64]> = cluster_data.chunks(shape).collect();
+        let old_bic = compute_bic(
+            &wrapped_cluster_data,
+            &vec![centroid],
+            vec![0; cluster_data.len()],
+        );
+        let new_bic = compute_bic(
+            &wrapped_cluster_data,
+            &kmeans_result.centroids.chunks(shape).collect(),
+            kmeans_result.assignments,
+        );
+        println!(
+            "Comparing centroids: {:?}({}) to {:?}({})?",
+            centroid, old_bic, kmeans_result.centroids, new_bic
+        );
+        if old_bic < new_bic {
+            next_centroids.extend(centroid.iter());
+            next_centroids.extend(kmeans_result.centroids.iter());
+        } else {
+        }
+    }
+
+    next_centroids
+}
