28195e3f8037d74167f0b16c41a9ff47670c9ebf
Mikolaj Figurski <miko.f80@gmail.com>
2024-08-31 18:00:05 -0400

    test: add likelihood, bic behavior tests

--- src/bic.rs
+++ src/bic.rs
@@ -45,11 +45,11 @@ fn compute_group_ll(errors: Vec<f64>, free_params: usize) -> f64 {
       return 0.0;
     }
     let distribution = Normal::new(0.0, std_dev).unwrap();
-    // println!("Distribution: {:?}", distribution);
+    println!("Distribution: {:?}", distribution);
     let ll = errors
         .iter()
         // folded distribution: double ll
-        .map(|x| 2.0 * distribution.ln_pdf(*x))
+        .map(|x| f64::ln(2.0) + distribution.ln_pdf(*x))
         .sum::<f64>();
     // println!("LL: {:?}", ll);
     ll
@@ -81,10 +81,24 @@ mod tests {
     #[test]
     fn it_computes_stddev() {
       // let data = vec![1.0, 2.0, 3.0, 4.0];
-      // let mean = 2.5;
+      // let data_mean = 2.5;
       let distance = vec![1.5, 0.5, 0.5, 1.5];
       let stddev = compute_stddev(&distance, 0);
       assert_eq!(stddev, 1.118033988749895,"Stddev should be ~1.118, got: {}", stddev);
+    }
+
+    #[test]
+    fn it_computes_likelihood() {
+      // let data = vec![1.0, 2.0, 3.0, 4.0];
+      // let data_mean = 2.5;
+      let distance = vec![1.5, 0.5, 0.5, 1.5];
+      // let distribution = Normal::new(0, 1.118)
+      // let approx_pdf = vec![0.322, 0.145, 0.145, 0.322];
+      // let approx_folded_pdf = vec![0.644, 0.290, 0.290, 0.644]
+      // let approx_multiplied = 0.0348
+      let l = f64::exp(compute_group_ll(distance, 0));
+      assert!(l > 0.0 && l < 1.0, "Likelihood should be in valid range, got: {}", l);
+      assert_eq!(l, 0.03510356758042785, "Likelihood should be ~.035, got: {}", l);
     }
 
     #[test]
@@ -96,5 +110,37 @@ mod tests {
       let bic = compute_bic(&wrapped, &state);
       assert!(bic > 0.0, "BIC should be > 0, got: {}", bic);
       assert!(bic < 10.0, "BIC should be < 10, got: {}", bic);
+    }
+
+    #[test]
+    fn it_computes_bic_penalizes_complexity() {
+      let data = vec![1.0, 1.1, 3.0, 3.1]; // note 2 clusters
+      let wrapped: Vec<&[f64]> = data.chunks(1).collect();
+
+      let good_state= KMeans::<_, 1>::new(data.clone(), 4, 1)
+        .kmeans_lloyd(2, 10, KMeans::init_random_partition, &KMeansConfig::default());
+      let good_bic = compute_bic(&wrapped, &good_state);
+
+      let complex_state = KMeans::<_, 1>::new(data.clone(), 4, 1)
+        .kmeans_lloyd(3, 10, KMeans::init_random_partition, &KMeansConfig::default());
+      let complex_bic = compute_bic(&wrapped, &complex_state);
+
+      assert!(good_bic < complex_bic, "BIC should discount complexity (good: {}, complex: {})", good_bic, complex_bic);
+    }
+
+    #[test]
+    fn it_computes_bic_penalizes_error() {
+      let data = vec![1.0, 1.1, 3.0, 3.1, 5.0, 5.1]; // note 3 clusters
+      let wrapped: Vec<&[f64]> = data.chunks(1).collect();
+
+      let good_state= KMeans::<_, 1>::new(data.clone(), 6, 1)
+        .kmeans_lloyd(3, 10, KMeans::init_random_partition, &KMeansConfig::default());
+      let good_bic = compute_bic(&wrapped, &good_state);
+
+      let error_state = KMeans::<_, 1>::new(data.clone(), 6, 1)
+        .kmeans_lloyd(2, 10, KMeans::init_random_partition, &KMeansConfig::default());
+      let error_bic = compute_bic(&wrapped, &error_state);
+
+      assert!(good_bic < error_bic, "BIC should discount high error (good: {}, error: {})", good_bic, error_bic);
     }
 }
